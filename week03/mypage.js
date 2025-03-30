5ccbef1c39e1a5dec5296da3eeb39f7eef844dea
seeun <seeun3139@sookmyung.ac.kr>
2025-03-30 17:46:46 +0900

    cursor

--- week03/mypage.js
+++ week03/mypage.js
@@ -1,11 +1,5 @@
 var scene = document.getElementById('scene');
 var parallax = new Parallax(scene);
-var isZoomed = false;
-
-function zoom_Img() {
-    var card = event.target.closest('.card');
-    card.classList.toggle('zoomin');
-}
 
 function flip_Img() {
     var card = event.target.closest('.card');
