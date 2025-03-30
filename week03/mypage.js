6b4415c09f0189376d73ad779c290fd0c80bb18b
seeun <seeun3139@sookmyung.ac.kr>
2025-03-30 14:48:06 +0900

    tarot_cards_sort

--- week03/mypage.js
+++ week03/mypage.js
@@ -2,6 +2,6 @@ var scene = document.getElementById('scene');
 var parallax = new Parallax(scene);
 
 function flip_Img() {
-    var card = document.getElementById('card');
+    var card = event.target.closest('.card');
     card.classList.toggle('clicked');
 }
