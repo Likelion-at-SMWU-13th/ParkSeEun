8be2ede10ed50e99aa5abc28d4841c7d4e81efd1
seeun <seeun3139@sookmyung.ac.kr>
2025-04-01 20:11:05 +0900

    static 폴더 및 파일 생성

---
+++ week04/static/js/signup.js
@@ -0,0 +1,11 @@
+function checkPassword() {
+    const pw1 = document.getElementById("password1").value;
+    const pw2 = document.getElementById("password2").value;   
+    
+    if (pw1 !== pw2){
+        alert("비밀번호가 다릅니다!");
+        return false;
+    }
+    window.location.href = "./main.html";
+    return false;
+}
