272bee58a4b6d80b88693ab158ed11f1f8bee514
seeun <seeun3139@sookmyung.ac.kr>
2025-04-13 14:08:13 +0900

    토탈 함수 작성

--- front/frontWeek01/week01_hw/randomVendingMachine.js
+++ front/frontWeek01/week01_hw/randomVendingMachine.js
@@ -1,12 +1,17 @@
 const drinks = [
-    { name: "cok", price: 1200 },
-    { name: "cider", price: 1200 },
-    { name: "fanta", price: 1100 },
-    { name: "sprite", price: 1300 },
-    { name: "milkis", price: 1000},
+    { name: "코카콜라", price: 1200 },
+    { name: "사이다", price: 1200 },
+    { name: "환타", price: 1100 },
+    { name: "스프라이트", price: 1300 },
+    { name: "밀키스", price: 1000},
+    { name: "레몬에이드", price: 1500},
+    { name: "닥터페퍼", price: 1400},
+    { name: "모구모구", price: 2000},
 ];
 
-let Money = 20000;
+let Money = 4000;
+let spendMoney = 0;
+let drunk = [];
 
 function randomDrink(arr) {
     return Math.floor(Math.random() * arr.length);
@@ -14,21 +19,37 @@ function randomDrink(arr) {
 
 function printDrink(drink) {
     if (Money >= drink.price){
-        console.log(`${drink.name} 음료가 나왔어요! (가격: ${drink.price}원)`);
+        console.log(`-> *${drink.name}* 음료가 나왔어요! (가격: ${drink.price}원)`);
         Money -= drink.price;
-        console.log(`지갑에 남은 돈: ${Money}원`);
+        drunk.push(drink.name);
+        spendMoney += drink.price;
+        console.log(`-> 지갑에 남은 돈: ${Money}원`);
     }
     else {
-        console.log("돈이 부족해요! 음료를 살 수 없어요!");
+        console.log("-> 돈이 부족해요! 음료를 살 수 없어요!");
     }
 }
 
 function buyDrink() {
     let drink;
-    for(i=0;i<3;i++){
+    for(i=0 ; i<3 ; i++){
+        console.log("[🥤음료를 뽑는 중🥤]");
         drink = randomDrink(drinks);
         printDrink(drinks[drink])
+        console.log("-----------------------------------------");
+    }
+}
+
+function totalDrink(){
+    console.log(`☝️ 총, ${spendMoney}원을 사용했어요.`);
+    console.log(`☝️ ${drunk} 음료를 GET!`);
+    if (Money == 0) {
+        console.log(`☝️ 이제 남은 돈이 없습니다...`);
+    }
+    else{
+        console.log(`☝️ 이제 ${Money}원이 남았어요.`);
     }
 }
 
 buyDrink();
+totalDrink();
