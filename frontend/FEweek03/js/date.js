16a4d6b6b03a6ef9fde486f6fec5ebb7ae3ec022
seeun <seeun3139@sookmyung.ac.kr>
2025-05-06 17:07:42 +0900

    타이머 함수 실습 완료

--- frontend/FEweek03/js/date.js
+++ frontend/FEweek03/js/date.js
@@ -9,3 +9,6 @@ const sec = date.getSeconds();
 
 console.log(`${year}년 ${month}월 ${day}일\n`);
 console.log(`${hour}시 ${min}분 ${sec}초\n`);
+
+const formattedDate = `${year}-${String(month).padStart(2, "0")}-${String(day).padStart(2, "0")}`;
+console.log(formattedDate)
