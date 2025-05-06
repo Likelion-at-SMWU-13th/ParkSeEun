f955be62a2201b4b9b2bc05c05a731fcce3993fc
seeun <seeun3139@sookmyung.ac.kr>
2025-05-06 16:54:52 +0900

    Date 실습 완료

---
+++ frontend/FEweek03/js/date.js
@@ -0,0 +1,11 @@
+const date = new Date();
+
+const year = date.getFullYear();
+const month = date.getMonth()+1;
+const day = date.getDate();
+const hour = date.getHours();
+const min = date.getMinutes();
+const sec = date.getSeconds();
+
+console.log(`${year}년 ${month}월 ${day}일\n`);
+console.log(`${hour}시 ${min}분 ${sec}초\n`);
