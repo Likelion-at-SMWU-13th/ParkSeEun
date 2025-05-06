7dcf7ab67e317be695e18f80f09e636a57ff79e7
seeun <seeun3139@sookmyung.ac.kr>
2025-05-06 18:04:57 +0900

    fetch API 실습 완료

--- frontend/FEweek03/js/date.js
+++ frontend/FEweek03/js/date.js
@@ -11,4 +11,4 @@ console.log(`${year}년 ${month}월 ${day}일\n`);
 console.log(`${hour}시 ${min}분 ${sec}초\n`);
 
 const formattedDate = `${year}-${String(month).padStart(2, "0")}-${String(day).padStart(2, "0")}`;
-console.log(formattedDate)
+console.log(formattedDate);
