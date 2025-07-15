b58f245e8c881b6eaeb041a897e93d6631cfb6c2
seeun <seeun3139@sookmyung.ac.kr>
2025-07-15 16:53:50 +0900

    [Feat] Redirection / greeting 생성 후 라우팅

--- frontend/FEweek08/week08/src/Login.jsx
+++ frontend/FEweek08/week08/src/Login.jsx
@@ -12,7 +12,7 @@ const Login = () => {
   };
 
   return (
-    <div>
+    <div className="login-box">
       <h2>로그인</h2>
       <div className="btns-container">
         <button onClick={handleKakao}>
