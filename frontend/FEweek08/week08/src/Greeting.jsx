3f1ea9cac0544d86d17bf186e80b29f2842f8799
seeun <seeun3139@sookmyung.ac.kr>
2025-07-20 14:24:32 +0900

    [Feat] OAuth 흐름 주석 추가

--- frontend/FEweek08/week08/src/Greeting.jsx
+++ frontend/FEweek08/week08/src/Greeting.jsx
@@ -1,26 +1,32 @@
 import React, { useEffect, useState } from "react";
 
+// 3. 리소스 서버의 자원 요청하기.
 const Greeting = () => {
   const [name, setName] = useState();
   const [profileImg, setProfileImg] = useState();
 
   useEffect(() => {
+    // localStorage에 저장한 Access Token을 가져옴.
     const accessToken = localStorage.getItem("accessToken");
+
+    // Access Token으로 카카오 서버에 사용자 정보 요청.
     fetch("https://kapi.kakao.com/v2/user/me", {
       method: "GET",
       headers: {
         Authorization: `Bearer ${accessToken}`,
         "Content-type": "application/x-www-form-urlencoded",
       },
+      // 사용자 닉네임과 프로필 이미지를 상태에 저장.
     }).then((res) => {
       const userData = res.json();
       userData.then((user) => {
-        setName(user.properties.nickname);
-        setProfileImg(user.properties.profile_image);
+        setName(user.properties.nickname); // 닉네임 상태 저장.
+        setProfileImg(user.properties.profile_image); // 프로필 상태 저장.
       });
     });
   }, []);
 
+  // 응답된 값을 UI에 반영.
   return (
     <div>
       <div
--- frontend/FEweek08/week08/src/Redirection.jsx
+++ frontend/FEweek08/week08/src/Redirection.jsx
@@ -1,14 +1,18 @@
 import React, { useEffect } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 
+// 2. Authorization Code를 사용해 Access Token 발급 받기.
 const Redirection = () => {
+  // Redirect URI에서 Authorization Code를 추출.
   const [params] = useSearchParams();
-
-  const grant_type = "authorization_code";
   const authCode = params.get("code");
+
+  // "authorization-code" 방식으로 토큰을 요청하겠다는 의미.
+  const grant_type = "authorization_code";
   const navigate = useNavigate();
 
   useEffect(() => {
+    // REST API KEY, Redirect URI, Authorization Code 값을 포함하여 Access Token을 요청.
     fetch(
       `https://kauth.kakao.com/oauth/token?grant_type=${grant_type}&client_id=${
         import.meta.env.VITE_REST_API_KEY
@@ -20,11 +24,12 @@ const Redirection = () => {
           "Content-Type": "application/x-www-form-urlencoded",
         },
       }
+      // 발급받은 Access Token을 localStorage에 저장한 뒤, greeting 페이지로 이동.
     ).then((res) => {
       const data = res.json();
       data.then((data) => {
-        localStorage.setItem("accessToken", data.access_token);
-        navigate("/greeting");
+        localStorage.setItem("accessToken", data.access_token); // 토큰 저장
+        navigate("/greeting"); // 페이지 이동
       });
     });
   }, [authCode, grant_type, navigate]);
