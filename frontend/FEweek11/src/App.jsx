a0a7ec31a8ede46ede9503f718bcb069ba2e6224
seeun <seeun3139@sookmyung.ac.kr>
2025-08-05 16:01:48 +0900

    [Feat] 카카오 지도 생성 기능 구현

--- frontend/FEweek11/src/App.jsx
+++ frontend/FEweek11/src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import MapContainer from "./components/MapContainer";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <MapContainer title="카카오 맵 실습" />
+    </>
+  );
 }
 
 export default App;
