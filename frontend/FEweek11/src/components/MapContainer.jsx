8e0e6b4443784ad4c8bf5fa808ba20c5449d3d96
seeun <seeun3139@sookmyung.ac.kr>
2025-08-05 16:52:37 +0900

    [Refactor] 카카오톡 공유 로직을 useKakaoShare hook으로 분리

--- frontend/FEweek11/src/components/MapContainer.jsx
+++ frontend/FEweek11/src/components/MapContainer.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import KakaoMap from "./KakaoMap";
 import * as S from "./styled";
 import { useState } from "react";
+import useKakaoShare from "../hooks/useKakaoShare";
 
 const MapContainer = ({ title }) => {
   const [center, setCenter] = useState({ lat: 33.450701, lng: 126.570667 });
   const [isMarker, setIsMarker] = useState(false);
-
-  const kakaoApiKey = import.meta.env.VITE_KAKAO_APP_KEY;
+  const { shareKakaoLocation } = useKakaoShare();
 
   const handleFindMyLocation = () => {
     navigator.geolocation.getCurrentPosition((pos) => {
@@ -19,40 +19,9 @@ const MapContainer = ({ title }) => {
   };
 
   const handleShareLocation = () => {
-    if (!window.Kakao.isInitialized()) {
-      window.Kakao.init(kakaoApiKey);
-    }
-
-    window.Kakao.Share.sendDefault({
-      objectType: "location",
-      address: `${center.lat}, ${center.lng}`,
-      addressTitle: "내 위치",
-      content: {
-        title: "숙명여대 멋사 13기 프론트 실습",
-        description: "나의 위치를 공유합니다.",
-        imageUrl:
-          "https://i.namu.wiki/i/o81kFfad6uoV0sN0iGSqA3hEKyIniZUhP7VBq2dI3WeyOFalhAEa_lQ_SY0GEbC_oJ4qzLlKokalWFk6cx86vg.webp",
-        link: {
-          mobileWebUrl: "https://developers.kakao.com",
-          webUrl: "https://developers.kakao.com",
-        },
-      },
-      social: {
-        likeCount: 2200,
-        commentCount: 45,
-        sharedCount: 845,
-      },
-      buttons: [
-        {
-          title: "웹으로 보기",
-          link: {
-            mobileWebUrl: "https://developers.kakao.com",
-            webUrl: "https://developers.kakao.com",
-          },
-        },
-      ],
-    });
+    shareKakaoLocation(center);
   };
+
   return (
     <S.Wrapper>
       <S.H1>{title}</S.H1>
