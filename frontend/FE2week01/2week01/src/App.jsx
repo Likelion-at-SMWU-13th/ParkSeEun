281339bcbc9269aefbeaa62a43b8b29bd7454004
seeun <tpdms3139@naver.com>
2025-09-02 19:46:58 +0900

    [Setting] 초기 코드 작성

--- frontend/FE2week01/2week01/src/App.jsx
+++ frontend/FE2week01/2week01/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
+import ToggleBtn from "./components/ToggleBtn";
 
 function App() {
   return (
     <>
       <Outlet />
+      <ToggleBtn />
     </>
   );
 }
