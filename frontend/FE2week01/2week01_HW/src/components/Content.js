8d11960b6bddf407492fa92102a73a3d4c5293f8
seeun <tpdms3139@naver.com>
2025-09-07 12:35:56 +0900

    [Refactor] useContent 삭제

--- frontend/FE2week01/2week01_HW/src/components/Content.js
+++ frontend/FE2week01/2week01_HW/src/components/Content.js
@@ -1,15 +1,12 @@
 import styled from "styled-components";
 import Left from "./Left";
 import Right from "./Right";
-import { useContent } from "../contexts/ContentContext";
-
 const Content = () => {
-  const { part, setPart } = useContent();
   return (
     <Wrapper>
       <div className="content">
-        <Left setPart={setPart} />
-        <Right part={part} />
+        <Left />
+        <Right />
       </div>
     </Wrapper>
   );
