2df430ad42cd13abb114d5b7c1074e759a9ea999
seeun <tpdms3139@naver.com>
2025-09-23 19:55:12 +0900

    [Feat] useQuery를 사용한 데이터 조회 완료

--- frontend/FE2week04/2week04/src/App.jsx
+++ frontend/FE2week04/2week04/src/App.jsx
@@ -1,7 +1,29 @@
+import { useQuery } from "@tanstack/react-query";
 import "./App.css";
 
 function App() {
-  return <>💗tanctack query 실습💗</>;
+  async function getPosts() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    return await response.json();
+  }
+  const { data: postsData } = useQuery({
+    queryKey: ["posts"],
+    queryFn: getPosts,
+  });
+  console.log("postsData", postsData);
+
+  const posts = postsData ?? [];
+
+  return (
+    <div>
+      💗tanctack query 실습💗
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
