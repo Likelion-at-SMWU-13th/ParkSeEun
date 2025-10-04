490d0cdd0a774fff07222d103c09defbe2ee219b
seeun <tpdms3139@naver.com>
2025-10-04 14:32:26 +0900

    [Feat] 인터페이스 및 함수 작성

--- frontend/FE2week05/2week05_hw/src/App.tsx
+++ frontend/FE2week05/2week05_hw/src/App.tsx
@@ -1,7 +1,32 @@
 import "./App.css";
 
+interface TeamLCK {
+  teamName: string;
+}
+
+interface TeamLCK {
+  teamColor: string;
+  lckRank: number;
+}
+
+interface WorldChampionShip {
+  isQualified: boolean;
+}
+
+interface LCKWorldChampionTeam extends TeamLCK, WorldChampionShip {
+  titles: number;
+}
+
+const WorldChampionTeam2025: LCKWorldChampionTeam = {
+  teamName: "T1",
+  teamColor: "red",
+  lckRank: 4,
+  isQualified: true,
+  titles: 5,
+};
+
 function App() {
-  return <></>;
+  console.log("2025 WorldChampionShip 진출팀:", WorldChampionTeam2025);
 }
 
 export default App;
