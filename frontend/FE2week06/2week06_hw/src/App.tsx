7430dcefb595ce6ac66f5156e412c57f654559dc
seeun <tpdms3139@naver.com>
2025-11-02 17:03:53 +0900

    [Feat] 경로 추가

--- frontend/FE2week06/2week06_hw/src/App.tsx
+++ frontend/FE2week06/2week06_hw/src/App.tsx
@@ -1,7 +1,17 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Melon from "./components/Melon";
+import Info from "./components/Info";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Melon />} />
+        <Route path="/info" element={<Info />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
